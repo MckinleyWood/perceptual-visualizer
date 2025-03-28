4e39910417b71b466e5ef8c7d440c15bac3c371f
MckinleyWood <mwood@dhdev.ca>
2025-03-28 10:27:19 -0700

    Added multi-file input / output

--- localizer.py
+++ localizer.py
@@ -1,8 +1,10 @@
 import argparse
 from dataclasses import dataclass, asdict
-import json
+import csv
+import glob
 import librosa
 import numpy as np
+import os
 
 import feature_extraction as fe
 
@@ -16,44 +18,37 @@ class DataFrame:
     strength: float
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename", type=str)
-    parser.add_argument("--duration", type=float, default=10)
-    parser.add_argument("--frame_length", type=int, default=8192)
-    parser.add_argument("--hop_length", type=int, default=4096)
-    parser.add_argument("--sr", type=int, default=22050)
-    args = parser.parse_args()
-
-    # y will be the input signal to analyze.
-    y, _ = librosa.load(args.filename, 
-        sr=args.sr, 
-        duration=args.duration, 
-        mono=False)
+def process_file(
+        filename: str, 
+        duration: int, 
+        frame_length: int, 
+        hop_length, 
+        sr: int) -> None:
+    """
+    Processes a file and extracts features."
+    """
+       # y will be the input signal to analyze.
+    y, _ = librosa.load(filename, sr=sr, duration=duration, mono=False)
     
     # D will be the (complex) STFT of y.
-    D = librosa.stft(
-        y,
-        n_fft=args.frame_length,
-        hop_length=args.hop_length, 
-        center=False)
+    D = librosa.stft(y, n_fft=frame_length, hop_length=hop_length, 
+                     center=False)
     
     # S will be the magnitude spectrum of y.
     S = np.abs(D)
 
     # We frame the signal y to match the frames of the STFT.
-    y = librosa.util.frame(
-        y, 
-        frame_length=args.frame_length, 
-        hop_length=args.hop_length)
-    y = y * np.hanning(args.frame_length)[None, :, None]
+    y = librosa.util.frame(y, frame_length=frame_length, hop_length=hop_length)
+    y = y * np.hanning(frame_length)[None, :, None]
 
+    # Calculate the features.
     volumes = fe.rms_volume(y)
-    x_pos = fe.x_pos(y, D, args.sr)
+    x_pos = fe.x_pos(y, D, sr)
     msws = fe.msw(y)
-    centroids = fe.centroid(S, args.sr)
-    bandwidths = fe.bandwidth(S, args.sr)
+    centroids = fe.centroid(S, sr)
+    bandwidths = fe.bandwidth(S, sr)
 
+    # Create a CSV file with the features.
     data_frames = [
         DataFrame(xp, yp, xw, yw, s)
         for xp, yp, xw, yw, s 
@@ -61,9 +56,43 @@ def main():
     ]
     
     data_dicts = [asdict(frame) for frame in data_frames]
-    with open("output.json", "w") as f:
-        json.dump(data_dicts, f, indent=2)
+    base = os.path.splitext(os.path.basename(filename))[0]
+    output_filename = "output/" + base + '.csv'
+    fieldnames = data_dicts[0].keys()
 
+    with open(output_filename, mode='w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data_dicts)
+
+    print(f"Processed '{filename}' -> '{output_filename}'")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path", type=str)
+    parser.add_argument("--duration", type=float, default=10)
+    parser.add_argument("--frame_length", type=int, default=8192)
+    parser.add_argument("--hop_length", type=int, default=4096)
+    parser.add_argument("--sr", type=int, default=22050)
+    args = parser.parse_args()
+
+    # Create the output directory if it doesn't exist.
+    if not os.path.exists("output"):
+        os.makedirs("output")
+
+    if os.path.isdir(args.input_path):
+        # If input_path is a folder, process all .wav files in it.
+        file_list = glob.glob(os.path.join(args.input_path, "*.wav"))
+    else:
+        # Otherwise, assume it's a single file.
+        file_list = [args.input_path]
+    
+    # Process each file.
+    for filename in file_list:
+        process_file(filename, args.duration, args.frame_length, 
+                     args.hop_length, args.sr)
+ 
 
 if __name__ == "__main__":
     main()
